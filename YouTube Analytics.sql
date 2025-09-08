cc489098427af27737b007467d4a1aaa59faa897
Andrea To Chip <andreatochip@yahoo.com>
2025-09-08 17:17:42 +0800

    Update YouTube Analytics.sql

--- YouTube Analytics.sql
+++ YouTube Analytics.sql
@@ -1,4 +1,4 @@
--- Create a table named Orders listing all information for each purchase
+-- Create a table named Analytics listing ageneral information for videos under a channel
 CREATE TABLE Analytics (
     Video_ID INT PRIMARY KEY,
     Title VARCHAR(50) NOT NULL,
@@ -6,3 +6,20 @@ CREATE TABLE Analytics (
     Upload_Date DATE NOT NULL,
     Duration_Seconds INT
 );
+
+-- Rename table Analytics to Video_Info
+EXEC sp_rename 'Analytics', 'Video_Info';
+
+-- Create another table named Performance_Metrics
+CREATE TABLE Performance_Metrics (
+    Views INT PRIMARY KEY,
+    Watch_Time_Minutes FLOAT NOT NULL,
+    avg_view_duration_seconds FLOAT,
+    Likes INT NOT NULL,
+    Dislikes INT NOT NULL
+);
+
+-- Add 2 columns to the Performance_Metrics Table
+ALTER TABLE Performance_Metrics
+ADD Comments_Count INT,
+ADD Shares INT;
