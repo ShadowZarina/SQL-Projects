3681593e8e8cf2d765446df19f131a1157082e9a
Andrea To Chip <andreatochip@yahoo.com>
2025-09-11 15:27:55 +0800

    Update YouTube Analytics.sql

--- YouTube Analytics.sql
+++ YouTube Analytics.sql
@@ -69,3 +69,102 @@ CREATE TABLE Audience (
     Audience_Retention_Rate INT,
     Click_Through_Rate INT
 );
+
+-- Select everything from the Orders table
+SELECT * 
+FROM Orders;
+
+-- Arrange all purchases by Customer ID number
+SELECT *
+FROM Orders
+ORDER BY ID ASC;
+
+-- Select everything from the Revenue table
+SELECT * 
+FROM Revenue;
+
+-- List all orders and the revenue gained together
+SELECT 
+    o.Order_ID, 
+    o.Customer_ID, 
+    o.Order_Date, 
+    o.Product, 
+    o.Quantity, 
+    o.Unit_Price, 
+    r.Total_Amount
+FROM Orders o
+JOIN Revenue r
+    ON o.Order_ID = r.Order_ID
+
+-- Update the tables to add a new customer purchase
+INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Product, Quantity, Unit_Price) VALUES 
+(11, 144629, '2025-07-07', 'Bluetooth Keyboard', 1, 35.00);
+
+INSERT INTO Revenue (Order_ID, Total_Amount) VALUES 
+(11, 35.00);
+
+-- List down the total revenue per customer
+SELECT 
+    o.Customer_ID, 
+    SUM(r.Total_Amount) AS Total_Spent
+FROM Orders o
+JOIN Revenue r ON o.Order_ID = r.Order_ID
+GROUP BY o.Customer_ID;
+
+-- Find the 2 most frequent customers in Orders
+SELECT TOP 2 
+    Customer_ID, 
+    COUNT(*) AS Order_Count
+FROM Orders
+GROUP BY Customer_ID
+ORDER BY Order_Count DESC
+
+-- Show all purchases made by Customer ID 144629
+SELECT *
+FROM Orders
+WHERE Customer_ID = 144629;
+
+-- List down the total revenue by day
+SELECT 
+    o.Order_Date,
+    SUM(r.Total_Amount) AS Daily_Revenue
+FROM Orders o
+JOIN Revenue r ON o.Order_ID = r.Order_ID
+GROUP BY o.Order_Date
+ORDER BY o.Order_Date;
+
+-- Show all customers who purchased a wireless mouse
+SELECT *
+FROM Orders
+WHERE Product = 'Wireless Mouse';
+
+-- Find the total revenue per product
+SELECT 
+    o.Product, 
+    SUM(r.Total_Amount) AS Total_Revenue
+FROM Orders o
+JOIN Revenue r ON o.Order_ID = r.Order_ID
+GROUP BY o.Product
+ORDER BY Total_Revenue DESC;
+
+-- Find most ordered items in Orders
+SELECT 
+    Product, 
+    SUM(Quantity) AS Total_Quantity
+FROM Orders
+GROUP BY Product
+ORDER BY Total_Quantity DESC;
+
+-- Create view of all purchases in descending order of revenue
+CREATE VIEW Purchases_By_Revenue AS
+SELECT 
+    o.Order_ID,
+    o.Customer_ID,
+    o.Order_Date,
+    o.Product,
+    o.Quantity,
+    o.Unit_Price,
+    r.Total_Amount
+FROM Orders o
+JOIN Revenue r ON o.Order_ID = r.Order_ID
+ORDER BY r.Total_Amount DESC;
